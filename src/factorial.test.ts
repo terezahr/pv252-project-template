82eb1f29b0da8f6cf4b8caa421db7c6db8037169
terezahr <91895985+terezahr@users.noreply.github.com>
2024-10-06 23:54:17 +0200

    add new unit tests

--- src/factorial.test.ts
+++ src/factorial.test.ts
@@ -10,3 +10,19 @@ test("factorial-minus", () => {
   };
   expect(will_throw).toThrow("Negative numbers not supported");
 });
+
+test("factorial-0", () => {
+  expect(factorial(0)).toBe(1);
+});
+
+test("factorial-1", () => {
+  expect(factorial(1)).toBe(1);
+});
+
+test("factorial-10", () => {
+  expect(factorial(10)).toBe(3628800);
+});
+
+test("factorial-1.5", () => {
+  expect(() => factorial(1.5)).toThrow("Negative numbers not supported");
+});
